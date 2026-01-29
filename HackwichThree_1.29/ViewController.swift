f92f6d6b5cb36fdf669d447e3224c46543a0937d
Bryleigh Ballesteros <bab3@hawaii.edu>
2026-01-29 12:19:35 -1000

    Completed Part 3 or Hackwich 3 in class practice checklist - need to complete part 5 & 6

--- HackwichThree_1.29/ViewController.swift
+++ HackwichThree_1.29/ViewController.swift
@@ -9,10 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Part 3,Task 1
+    var stringOne: String = "Red"
+    var stringTwo: String = "Blue"
+    //Part 5, Task 1
+    var stringThree: String = "cool"
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //Part 3,Task 3
+    @IBAction func changeColorButton(_ sender: Any) {
+        //Part3,Task 4
+        if stringOne = "Blue" {
+            view.backgroundColor = .red
+            //Example from class: self.view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = .blue
+
+        }
+        
+    //Part 5,Task 3
+    @IBOutlet weak var changeColor: UIButton!
+    
+    
+    
+    
+   
 
 
 }
